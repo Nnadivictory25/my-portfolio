a0e493e0819d8bc2c55ee05fbc378b117a176d48
Nnadi Victory <chikamagozi25@gmail.com>
2025-08-12 09:35:19 +0100

    Add configuration for Node.js File API polyfills and optimize dependencies in vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -3,5 +3,17 @@ import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vite';
 
 export default defineConfig({
-	plugins: [tailwindcss(), sveltekit()]
+	plugins: [tailwindcss(), sveltekit()],
+	define: {
+		// Polyfill File API for Node.js environment
+		'global.File': 'undefined',
+		'global.FileReader': 'undefined',
+		'global.Blob': 'undefined'
+	},
+	optimizeDeps: {
+		exclude: ['undici']
+	},
+	ssr: {
+		noExternal: ['undici']
+	}
 });
