e75e147730e2144f2a2efb90a00fdcb4a139fae7
Nnadi Victory <chikamagozi25@gmail.com>
2025-08-12 10:08:03 +0100

    Update Svelte adapter from Cloudflare to auto and upgrade devDependency version in package.json

--- svelte.config.js
+++ svelte.config.js
@@ -1,4 +1,4 @@
-import adapter from '@sveltejs/adapter-cloudflare';
+import adapter from '@sveltejs/adapter-auto';
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 
 /** @type {import('@sveltejs/kit').Config} */
@@ -8,22 +8,7 @@ const config = {
 	preprocess: vitePreprocess(),
 
 	kit: {
-		// adapter-auto only supports some environments, see https://svelte.dev/docs/kit/adapter-auto for a list.
-		// If your environment is not supported, or you settled on a specific environment, switch out the adapter.
-		// See https://sveltejs.dev/docs/kit/adapters for more information about adapters.
-		adapter: adapter({
-			platformProxy: true,
-			routes: {
-				include: ['/*'],
-				exclude: ['<all>']
-			}
-		}),
-		env: {
-			dir: process.cwd()
-		},
-		alias: {
-			$lib: './src/lib'
-		}
+		adapter: adapter()
 	}
 };
 
